7657b8acb40824020dfa0305d0ce45f75548dc72
Ashley Casey <acasey.dev@gmail.com>
2018-01-15 21:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length == 0) {
-    return 'Your shopping cart is empty.'
+    return console.log("Your shopping cart is empty.")
   } else {
     let newCart = [];
     for (var i = 0; i < cart.length; i++) {
